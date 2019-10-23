e04ab70c0939e678c6a73d121389b656221049d5
unknown <frankodey2@gmail.com>
2019-10-23 20:33:04 +0100

    Project: [Setup: Backend and Frontend]

---
+++ app/index.js
@@ -0,0 +1,23 @@
+const express = require('express') ,
+morgan = require('morgan'),
+cors = require('cors'),
+axios = require('axios')
+
+const app = express();
+
+const origin = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'prod-url'
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors({ origin }));
+
+app.get('/', async(req, res) => {
+    const count = req.query.count || 20
+    const response = await axios.get(`https://randomuser.me/api?results=${count}`);
+    res.json({ data: response.data.results}); 
+});
+
+app.listen(4000, () => {
+    console.log(`Listening on PORT 4000`)
+
+});
